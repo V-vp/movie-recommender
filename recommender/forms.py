49173646a2bd9f971f0565a5253b09dd456acd7d
V-vp <pawarvivek2705@gmail.com>
2019-01-21 13:41:18 +0530

    updated forms.py

--- recommender/forms.py
+++ recommender/forms.py
@@ -2,7 +2,28 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(
+    attrs = {
+        'placeholder' : 'Password',
+        'class':'form-control',
+        'required':'required'
+    }
+    ))
+    username = forms.CharField(widget=forms.TextInput(
+    attrs = {
+        'placeholder' : 'Username',
+        'class':'form-control',
+        'required':'required'
+    }
+    ))
+    email = forms.CharField(widget=forms.TextInput(
+    attrs = {
+        'placeholder' : 'Email',
+        'class':'form-control',
+        'type':'email',
+        'required':'required'
+    }
+    ))
 
     class Meta:
         model = User
